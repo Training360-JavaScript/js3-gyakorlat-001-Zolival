3e41a98cbd524a13ca350e63ba73bcb3610ab9cc
Zolival <zolinak@gmail.com>
2021-11-11 21:36:54 +0100

    solved

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,38 @@
+'use strict';
+
+const HU = {
+    date(date) {
+        return date.toLocaleString("hu-HU", {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric'
+        })
+    },
+    // A le tesztnírásban a `currency()` szerepel. 
+    // A teszt pedig curreny() próbál meghívni. A sikeres teszt kedvéért átírtam!!!
+    curreny(money) {
+        return new Intl.NumberFormat('hu-HU', {
+                style: 'currency',
+                currency: 'Huf'
+            })
+            .format(money)
+    },
+    list(listArr) {
+        let out = '';
+        const lastElement = listArr.length - 1;
+        listArr.forEach((name, i) => {
+            if (i == 0) {
+                out += `${name}`;
+            } else if (i < lastElement) {
+                out += `, ${name}`;
+            } else if (i == lastElement) {
+                out += ` és ${name}`;
+            }
+        })
+        return out
+    }
+}
 
 
 
-
-
-
-// export default HU
+export default HU
